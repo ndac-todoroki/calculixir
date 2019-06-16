a750e69083221104ef99cde1db46e906fa726b8d
todoroki <ryo.konishi@mixi.co.jp>
2019-06-16 15:40:15 +0900

    add Stage (wip)

---
+++ lib/calculator/stage.ex
@@ -0,0 +1,10 @@
+defmodule Calculator.Stage do
+  @moduledoc """
+  計算機の各ステージを持つかもしれない
+  """
+
+  @type t :: __MODULE__
+
+  def new(string) do
+  end
+end
---
+++ test/calculator/stage_test.exs
@@ -0,0 +1,5 @@
+defmodule Calculator.StageTest do
+  use ExUnit.Case, async: true
+
+  @module Calculator.Stage
+end
